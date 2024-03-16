3b0b0fc4f1e35fc5e276d2222652ef0c55c4a4e7
Hasan Rahic <haso.raha10@gmail.com>
2024-03-16 19:29:27 +0100

    pushing files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div>
+      <div className="main">
+        <div className="header">
+          <img src="" alt="" />
+          <p className="name">Hasan Rahic</p>
+          <p className="age">20 years old</p>
+          <p className="about">lasdkjflasdjflasjdfkl</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
