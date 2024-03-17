2334d0c6dbcd825f1a3c87c3c92820fc84953123
Hasan Rahic <haso.raha10@gmail.com>
2024-03-17 15:36:18 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
       <div className="main">
         <div className="header">
-          <img src="" alt="" />
-          <p className="name">Hasan Rahic</p>
-          <p className="age">20 years old</p>
-          <p className="about">lasdkjflasdjflasjdfkl</p>
+          <img src="./images/Hasan-Rahic-PNG.png" alt="" />
+          <div className="text">
+            <p className="name">Hasan Rahic</p>
+            <p className="age">20 years old</p>
+            <p className="about">Second-year student at Faculty of Information Technologies</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
