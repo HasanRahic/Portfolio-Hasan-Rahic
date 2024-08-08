be4baf1cf515ae51f5ac85edcbc83673aec132ab
Hasan Rahic <haso.raha10@gmail.com>
2024-08-08 11:58:03 +0200

    centering svgs

--- src/Components/Connect/Connect.jsx
+++ src/Components/Connect/Connect.jsx
@@ -14,13 +14,13 @@ const Connect = () => {
             <div className="github">
               <AnimatedOnScroll animationIn="fadeInLeft">
                 <p>Discover a collection of my past projects and contributions on GitHub. Click the button below to explore:</p>
-                <a href="https://github.com/HasanRahic"><button ><FaGithub /> GitHub</button></a>
+                <a href="https://github.com/HasanRahic"><button className='icon-button-center'><FaGithub /> GitHub</button></a>
               </AnimatedOnScroll>
             </div>
             <div className="linkedin">
               <AnimatedOnScroll animationIn="fadeInRight">
                 <p>Let's connect on LinkedIn! Explore my professional journey, accomplishments, and interests by clicking the button below:</p>
-                <a href="https://www.linkedin.com/in/hasan-rahic-3941b2255/"><button><FaLinkedin /> LinkedIn</button></a>
+                <a href="https://www.linkedin.com/in/hasan-rahic-3941b2255/"><button className='icon-button-center'><FaLinkedin /> LinkedIn</button></a>
               </AnimatedOnScroll>
             </div>
             <div className="mail">
@@ -28,7 +28,7 @@ const Connect = () => {
                 <p>Let's Connect and Discuss Opportunities!
                   Interested in learning more about my experience, discussing potential collaborations, or exploring new opportunities together? Feel free to reach out to me via email! Click the button below to send me a message:
                   Looking forward to connecting and hearing from you soon!</p>
-                <a href="mailto:hasanrahic604@gmail.com"><button><FaEnvelope /> Mail</button></a>
+                <a href="mailto:hasanrahic604@gmail.com"><button className='icon-button-center'><FaEnvelope /> Mail</button></a>
               </AnimatedOnScroll>
             </div>
           </div>
